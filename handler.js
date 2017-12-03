3596a3079c8275dc17d819e17e2e17057e653af1
Mitch Gavan <mitchgavan@gmail.com>
2017-12-03 17:43:22 +1100

    added getGame handler

--- handler.js
+++ handler.js
@@ -1,5 +1,40 @@
 'use strict';
 
+const mongoose = require('mongoose')
+const bluebird = require('bluebird')
+const GameModel = require('./model/Game')
+
+mongoose.Promise = bluebird
+
+const MONGO_STRING = process.env.MONGODB_STRING
+
+const errorResponse = (statusCode, message) => ({
+  statusCode: statusCode || 501,
+  headers: { 'Content-Type': 'text/plain' },
+  body: message || 'Incorrect Id',
+});
+
+module.exports.getGame = (event, context, callback) => {
+  const db = mongoose.connect(MONGO_STRING).connection
+  const id = event.pathParameters.id;
+
+  db.once('open', () => {
+    GameModel.findOne({ id: id })
+      .then((game) => {
+        callback(null, { 
+          statusCode: 200,
+          body: JSON.stringify(game)
+        })
+      })
+      .catch((err) => {
+        callback(null, errorResponse(400, 'No game with that ID found'))
+      })
+      .finally(() => {
+        db.close()
+      })
+  })
+}
+
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
